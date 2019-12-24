013f0f2a04650a7cee8d83f8ab2138d041d1c902
RENI MIKAZUKI <58826950+ReniRails@users.noreply.github.com>
2019-12-24 22:05:21 +0700

    Add files via upload

---
+++ ktp.class.php
@@ -0,0 +1,37 @@
+<?php 
+
+class Constans
+{
+	const api_endpoint	= 'https://www.renirails.io';
+	const api_password	= 'ReniRails (https://www.renirails.io)';
+}
+
+class Request
+{
+	public function CurlGet($url, $headers){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+	}
+}
+
+class KTP{
+	
+	protected $nik;
+	
+	public function __construct($nik = null){
+		$this->nik = $nik;
+	}
+	
+	public function check(){
+		$headers = array(
+			'request_key: ' . Constans::api_password,
+		);
+		return Request::CurlGet(Constans::api_endpoint . "/api/v2.0/e-ktp/" . $this->nik, $headers);
+	}
+}
